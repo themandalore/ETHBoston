d6c6d070111a8ee7c2620fe96aac2f3aaf947f34
Nicholas Fett <nicholasfett@Nicholass-MacBook-Air.local>
2024-04-28 09:01:16 -0400

    n

--- scripts/uploadFile.js
+++ scripts/uploadFile.js
@@ -2,18 +2,19 @@ const fs = require('fs').promises;
 
 
 async function main(){
-    const data = await fs.readFile('./secret.txt', 'utf8'); //nodejs specific
-    const response = await fetch('https://api.akord.com/files', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Api-Key': 'your_api_key',
-            'Content-Type': 'text/plain'
-        },
-        body: data
-    })
+    const data = await fs.readFile('./secret.png', 'utf8'); //nodejs specific
+    console.log(Buffer.from(data))
+    // const response = await fetch('https://api.akord.com/files', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Api-Key': 'your_api_key',
+    //         'Content-Type': 'text/plain'
+    //     },
+    //     body: data
+    // })
     
-    console.log(response)
+    // console.log(response)
 }
 
 main()
